8beab2d6fbbf8e498a1ff2bcd92da5a43ab8043d
Tushar Arora <Tushararora@gmail.com>
2021-01-03 15:56:28 +0530

    Both Digital And Analog Clocks are ready

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ $(document).ready(
             var hours=date.getHours();
             var minutes=date.getMinutes();
             var seconds=date.getSeconds();
+            $("#digitalclock > p").html(String(hours)+":"+String(minutes)+":"+String(seconds));
             $("#timetext").html("<p>Current Time is "+String(hours)+" hours "+String(minutes)+" minutes "+String(seconds)+" second </p>")
             $("#hourhand,#minutehand,#secondhand").css("transform-origin","bottom");
             $("#hourhand").css("transform","rotate(0deg)");
@@ -18,7 +19,7 @@ $(document).ready(
             var hourdegree=String((30*(hours%12))+(minutes*0.5))+"deg";
             var minutedegree=String((6*minutes)+(0.1*seconds))+"deg";
             var seconddegree=String(6*seconds)+"deg";
-           
+            
             $("#hourhand").css("transform","rotate("+hourdegree+")");
             $("#minutehand").css("transform","rotate("+minutedegree+")");
             $("#secondhand").css("transform","rotate("+seconddegree+")");
